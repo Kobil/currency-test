6607b2f87ad0b8b9413e12cc9a7f9a774d1f0cca
kobil <ko6a93@bk.ru>
2018-05-03 08:07:27 +0500

    main

--- index.js
+++ index.js
@@ -1,48 +1,9 @@
-const functions = require('firebase-functions');
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-
-var util = require('util');
-
-const admin = require('firebase-admin');
-admin.initializeApp(functions.config().firebase);
-
-var db = admin.firestore();
-
-var nbt = require("./nbt");
-var tsb = require("./tsb");
-
-var scrape = require('website-scraper');
-
-var options = {
-  urls: ['http://nodejs.org/'],
-  directory: '/path/to/save/',
-};
+var request = require("request-promise");
  
-
-
-exports.helloWorld = functions.https.onRequest((req, res) => {
-    console.log("Currency Started");
-   
-    // nbt.run(db);
-    //tsb.run(db);
-
-        // with promise
-    scrape(options).then((result) => {
-        /* some code here */
-        console.log(util.inspect(result, false, null));
-        return result;
-    }).catch((err) => {
-        /* some code here */
-        console.error(err);
-    });
-    res.send("Hello from Firebase!");
-    console.log("Currency Finished");
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
-
 
